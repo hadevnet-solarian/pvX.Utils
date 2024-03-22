c43b5909a577e70b4d5bda5245543d4863398dad
Hatef Ghani <hatef.ghani@solarian.com.tr>
2024-03-22 14:18:20 +0300

    - Some fixes

---
+++ Utils/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("pvX.Utils")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("pvX")]
+[assembly: AssemblyProduct("pvX")]
+[assembly: AssemblyCopyright("Copyright © pvX 2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("c6783d73-7c70-455d-a952-6ee574581a94")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
